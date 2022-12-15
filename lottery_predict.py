4340137e5384366d7e8462138c535c374d1f8ca6
khyojun <nandong5923@naver.com>
2022-12-15 17:23:29 +0900

    first commit

---
+++ lottery_predict.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import numpy as np
+rows = np.loadtxt("C:/Users/nando/Desktop/lotto.csv", delimiter=",")
+row_count=len(rows)
+print(row_count)
